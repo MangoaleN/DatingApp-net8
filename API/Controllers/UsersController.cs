b3df1680fd477bd3a6118f6daa12adb5e05711c9
MangoaleN <mangoale.n17@gmail.com>
2024-10-04 11:36:12 +0200

    Section 13 end

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using API.Extensions;
+using API.Helpers;
 
 namespace API.Controllers;
 
@@ -13,19 +14,22 @@ namespace API.Controllers;
 public class UsersController(IUserRepository userRepository, IMapper mapper,
  IPhotoService photoService) : BaseApiController
 {
-    
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
-    {
-        var users = await userRepository.GetMembersAsync();
 
-       return Ok(users);
-    }
-    
-    [HttpGet("{username}")]
-    public async  Task<ActionResult<MemberDto>> GetUser(string username)
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
     {
-        var user = await userRepository.GetMemberAsnyc(username);    
+        userParams.CurrentUsername = User.GetUsername();
+        var users = await userRepository.GetMembersAsync(userParams);
+
+        Response.AddPaginationHeader(users);
+
+        return Ok(users);
+    }
+
+    [HttpGet("{username}")]
+    public async Task<ActionResult<MemberDto>> GetUser(string username)
+    {
+        var user = await userRepository.GetMemberAsnyc(username);
 
         if (user == null) return NotFound();
 
@@ -37,11 +41,11 @@ public class UsersController(IUserRepository userRepository, IMapper mapper,
     {
         var user = await userRepository.GetUserByUsernameAsync(User.GetUsername());
 
-        if(user == null) return BadRequest("Could not find user");
+        if (user == null) return BadRequest("Could not find user");
 
         mapper.Map(memberUpdateDto, user);
 
-        if(await userRepository.SaveAllAsync()) return NoContent();
+        if (await userRepository.SaveAllAsync()) return NoContent();
 
         return BadRequest("Failed to update the user");
     }
@@ -49,30 +53,30 @@ public class UsersController(IUserRepository userRepository, IMapper mapper,
     [HttpPost("add-photo")]
     public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
     {
-            var user = await userRepository.GetUserByUsernameAsync(User.GetUsername());
+        var user = await userRepository.GetUserByUsernameAsync(User.GetUsername());
 
-            if(user == null) return BadRequest("Cannot update user");
+        if (user == null) return BadRequest("Cannot update user");
 
-            var result = await photoService.AddPhotoAsync(file);
+        var result = await photoService.AddPhotoAsync(file);
 
-            if(result.Error != null) return BadRequest(result.Error.Message);
+        if (result.Error != null) return BadRequest(result.Error.Message);
 
-            var photo = new Photo
-            {
-                Url = result.SecureUrl.AbsoluteUri,
-                PublicId = result.PublicId,
-            };
+        var photo = new Photo
+        {
+            Url = result.SecureUrl.AbsoluteUri,
+            PublicId = result.PublicId,
+        };
 
-            if(user.Photos.Count == 0) photo.IsMain = true;
-            
-            user.Photos.Add(photo);
+        if (user.Photos.Count == 0) photo.IsMain = true;
 
-            if(await userRepository.SaveAllAsync()) 
-            return CreatedAtAction(nameof(GetUser), 
-            new {username = user.UserName}, mapper.Map<PhotoDto>(photo));
+        user.Photos.Add(photo);
 
-            return BadRequest("Problem adding Photo");
-    }   
+        if (await userRepository.SaveAllAsync())
+            return CreatedAtAction(nameof(GetUser),
+            new { username = user.UserName }, mapper.Map<PhotoDto>(photo));
+
+        return BadRequest("Problem adding Photo");
+    }
 
     [HttpPut("set-main-photo/{photoId:int}")]
 
@@ -80,40 +84,41 @@ public class UsersController(IUserRepository userRepository, IMapper mapper,
     {
         var user = await userRepository.GetUserByUsernameAsync(User.GetUsername());
 
-        if(user == null) return BadRequest("Could not find user");
+        if (user == null) return BadRequest("Could not find user");
 
         var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
 
-        if(photo == null || photo.IsMain) return BadRequest("Cannot use main photo");
+        if (photo == null || photo.IsMain) return BadRequest("Cannot use main photo");
 
         var currentMain = user.Photos.FirstOrDefault(x => x.IsMain);
-        if(currentMain != null) currentMain.IsMain = false;
+        if (currentMain != null) currentMain.IsMain = false;
         photo.IsMain = true;
 
-        if(await userRepository.SaveAllAsync()) return NoContent();
+        if (await userRepository.SaveAllAsync()) return NoContent();
 
         return BadRequest("Problem setting main photo");
-         
+
     }
     [HttpDelete("delete-photo/{photoId}")]
     public async Task<ActionResult> Delete(int photoId)
     {
         var user = await userRepository.GetUserByUsernameAsync(User.GetUsername());
 
-        if(user == null) return BadRequest("user not found");
+        if (user == null) return BadRequest("user not found");
 
-        var photo = user.Photos.FirstOrDefault(x =>  x.Id== photoId);
+        var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
 
-        if(photo == null || photo.IsMain) return BadRequest("This photo cannot be deleted");
+        if (photo == null || photo.IsMain) return BadRequest("This photo cannot be deleted");
 
-        if(photo.PublicId != null){
+        if (photo.PublicId != null)
+        {
             var result = await photoService.DeletePhotoAsync(photo.PublicId);
-            if(result.Error != null) return BadRequest(result.Error.Message);
-        } 
+            if (result.Error != null) return BadRequest(result.Error.Message);
+        }
 
         user.Photos.Remove(photo);
 
-        if(await userRepository.SaveAllAsync()) return Ok();
+        if (await userRepository.SaveAllAsync()) return Ok();
 
         return BadRequest("Problem deleting photo");
     }
--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,9 +1,12 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
+import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
+import { inject, Injectable, model, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
 import { of, tap } from 'rxjs';
 import { Photo } from '../_models/Photo';
+import { PaginatedResult } from '../_models/pagination';
+import { UserParams } from '../_models/userParams';
+import { AccountsService } from './accounts.service';
 
 
 @Injectable({
@@ -11,56 +14,98 @@ import { Photo } from '../_models/Photo';
 })
 export class MembersService {
   private http = inject(HttpClient);
+  private accountService = inject(AccountsService);
   baseUrl = environment.apiUrl;
-  members = signal<Member[]>([]);
+  paginatedResult = signal<PaginatedResult<Member[]> | null>(null);
+  memberCache = new Map();
+  user = this.accountService.currentUser();
+  userParams = signal<UserParams>(new UserParams(this.user));
 
+  resetUserParams(){
+    this.userParams.set(new UserParams(this.user));
+  }
 
- getMembers(){
-  return this.http.get<Member[]>(this.baseUrl + 'users').subscribe({
-    next: members => this.members.set(members)
-  })
- }
+  getMembers() {
+    const response = this.memberCache.get(Object.values(this.userParams()).join('-'));
 
- getMember(username: string){
-  const member = this.members().find(x => x.userName === username);
+    if(response) return this.setPaginatedResponse(response);
 
-  if(member!== undefined) return of(member);
+    
+    let params = this.setPaginationHeaders(this.userParams().pageNumber, this.userParams().pageSize);
 
-  return this.http.get<Member>(this.baseUrl + 'users/' + username);
- }
- 
- updateMember(member: Member){
-      return this.http.put(this.baseUrl + 'users', member).pipe(
-        tap(() => {
-          this.members.update(members => members.map(m => m.userName === member.userName 
-            ? member : m))
-        })
-      )
- }
+    params = params.append('minAge', this.userParams().minAge);
+    params = params.append('maxAge', this.userParams().maxAge);
+    params = params.append('gender', this.userParams().gender);
+    params = params.append('orderBy', this.userParams().orderBy);
 
- setMainPhoto(photo: Photo){
-    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photo.id, {}).pipe(
-      tap(() => {
-        this.members.update(members => members.map(m => {
-          if(m.photos.includes(photo)){
-            m.photoUrl = photo.url
-          }
-          return m;
-        }))
-      })
-    )
- }
-
- deletePhoto(photo: Photo){
-  return this.http.delete(this.baseUrl + 'users/delete-photo/' + photo.id).pipe(
-   tap(() => {
-    this.members.update(members => members.map(m => {
-      if(m.photos.includes(photo)) {
-        m.photos = m.photos.filter(x => x.id !== photo.id)
+    return this.http.get<Member[]>(this.baseUrl + 'users', { observe: 'response', params }).subscribe({
+      next: response => {
+        this.setPaginatedResponse(response);
+        this.memberCache.set(Object.values(this.userParams()).join('-'), response);
       }
-      return m;
-    }))
-   })
-  )
- }
+    })
+  }
+
+  private setPaginatedResponse(response: HttpResponse<Member[]>){
+    this.paginatedResult.set({
+      items: response.body as Member[],
+      pagination: JSON.parse(response.headers.get('Pagination')!)
+    })
+  }
+
+  private setPaginationHeaders(pageNumber: number, pageSize: number) {
+    let params = new HttpParams();
+
+    if (pageNumber && pageSize) {
+      params = params.append('pageNumber', pageNumber);
+      params = params.append('pageSize', pageSize);
+    }
+
+    return params;
+  }
+
+  getMember(username: string) {
+    const member: Member = [...this.memberCache.values()]
+    .reduce((arr, elem) => arr.concate(elem.body), [])
+    .find((m: Member) => m.userName === username);
+
+    if(member) return of(member);
+
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      // tap(() => {
+      //  this.members.update(members => members.map(m => m.userName === member.userName 
+      //    ? member : m))
+      //})
+    )
+  }
+
+  setMainPhoto(photo: Photo) {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photo.id, {}).pipe(
+      //tap(() => {
+      // this.members.update(members => members.map(m => {
+      //   if(m.photos.includes(photo)){
+      //    m.photoUrl = photo.url
+      // }
+      //   return m;
+      //  }))
+      // })
+    )
+  }
+
+  deletePhoto(photo: Photo) {
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photo.id).pipe(
+      //tap(() => {
+      //this.members.update(members => members.map(m => {
+      //  if(m.photos.includes(photo)) {
+      //    m.photos = m.photos.filter(x => x.id !== photo.id)
+      // }
+      // return m;
+      //}))
+      //  })
+    )
+  }
 }
